8081c152be1646fb975a283dae9614009015dd7f
Bohdon Sayre <bohdon@gmail.com>
2023-10-03 20:24:23 -0400

    add GameplayAbilityItems plugin

---
+++ Plugins/GameplayAbilityItems/Source/GameplayAbilityItems/GameplayAbilityItems.Build.cs
@@ -0,0 +1,26 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GameplayAbilityItems : ModuleRules
+{
+	public GameplayAbilityItems(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+		});
+
+
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"CoreUObject",
+			"Engine",
+			"Slate",
+			"SlateCore",
+		});
+	}
+}
---
+++ Plugins/GameplayAbilityItems/Source/GameplayAbilityItems/Private/GameplayAbilityItemsModule.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameplayAbilityItemsModule.h"
+
+#define LOCTEXT_NAMESPACE "FGameplayAbilityItemsModule"
+
+void FGameplayAbilityItemsModule::StartupModule()
+{
+}
+
+void FGameplayAbilityItemsModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FGameplayAbilityItemsModule, GameplayAbilityItems)
