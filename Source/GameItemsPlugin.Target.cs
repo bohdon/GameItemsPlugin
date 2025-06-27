e68195c2d7f510b840c8ee9cfed354288210ca22
Bohdon Sayre <bohdon@gmail.com>
2025-06-27 16:03:42 -0400

    update sample project to 5.6

--- Source/GameItemsPlugin.Target.cs
+++ Source/GameItemsPlugin.Target.cs
@@ -7,7 +7,7 @@ public class GameItemsPluginTarget : TargetRules
 	public GameItemsPluginTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange(new string[]
--- Source/GameItemsPluginEditor.Target.cs
+++ Source/GameItemsPluginEditor.Target.cs
@@ -7,7 +7,7 @@ public class GameItemsPluginEditorTarget : TargetRules
 	public GameItemsPluginEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange(new string[]
